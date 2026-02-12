2319c7ab7cd3dd810a36661ae6bd17872ee217e2
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-11 22:43:11 -0500

    Gift start

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let currentPage = 0;
+const pages = document.querySelectorAll('.page');
+
+function nextPage() {
+    if (currentPage < pages.length - 1) {
+        pages[currentPage].classList.add('flipped');
+        pages[currentPage].classList.remove('active');
+        currentPage++;
+        pages[currentPage].classList.add('active');
+    }
+}
+
+function prevPage() {
+    if (currentPage > 0) {
+        pages[currentPage].classList.remove('active');
+        currentPage--;
+        pages[currentPage].classList.add('active');
+        pages[currentPage].classList.remove('flipped');
+    }
+}
